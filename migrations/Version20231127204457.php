7d9c0ba5aab48eefffbc6e1220ffd09dc36e98a0
Rodolphe Durand <rodolphe.durand@etu.univ-tours.fr>
2023-12-10 20:59:44 +0100

    Revert "Suppression de fichiers inutile"
    This reverts commit dea220f8e9d203a6e8b3b11a9d3290cc3d2b7e2c.

---
+++ migrations/Version20231127204457.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231127204457 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE admin');
+        $this->addSql('ALTER TABLE gif ADD gif_filename VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL COMMENT \'(DC2Type:json)\', CHANGE pseudo pseudo VARCHAR(180) NOT NULL, CHANGE role password VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64986CC499D ON user (pseudo)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_880E0D7686CC499D (pseudo), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE gif DROP gif_filename');
+        $this->addSql('DROP INDEX UNIQ_8D93D64986CC499D ON user');
+        $this->addSql('ALTER TABLE user DROP roles, CHANGE pseudo pseudo VARCHAR(255) NOT NULL, CHANGE password role VARCHAR(255) NOT NULL');
+    }
+}
