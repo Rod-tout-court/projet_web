89309a6aa479445ab06c8dff269df2fa998e1aa1
Rodolphe Durand <rodolphe.durand@etu.univ-tours.fr>
2023-12-10 21:14:29 +0100

    Modification pour compatibilité avec Postgresql

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[ORM\Table(name: '`user`')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['pseudo'], message: 'There is already an account with this pseudo')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
